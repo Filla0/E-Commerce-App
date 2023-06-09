5de5fb5b40fa1801e1756a84c467cf28b273880e
Pajer <110721437+Pajer3@users.noreply.github.com>
2023-06-09 12:43:51 +0200

    Slideshow and renamed somethings

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Slider from 'react-slick';
 import ProductCard from './ProductCard';
 import Header from './Header';
+import './HomeStyle.css';
 
 function Home() {
   // Sample product data (replace with your own product data)
@@ -20,16 +22,64 @@ function Home() {
     // Add more product objects as needed
   ];
 
+  // Configuration for the React Slick slideshow
+  const slideshowSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ],
+    appendDots: dots => (
+      <div>
+        <ul style={{ margin: "0px", padding: "0px" }}> {dots} </ul>
+      </div>
+    ),
+  };
+
   return (
     <div className="home">
-      <Header/>
-      <h1 style={{ textAlign: 'center' }}>Welcome to our E-commerce Store</h1>
+      <Header />
+      <h1 className="title">Quip</h1>
 
-      <h2 style={{ textAlign: 'center' }}>Featured Products</h2>
-      <div className="product-list" style={{ display: 'flex', justifyContent: 'center' }}>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+      <h2 className="subtitle">Top Picks</h2>
+      <div className="product-list">
+        <Slider {...slideshowSettings}>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </Slider>
+      </div>
+
+      <div className="bar"></div>
+
+      <h2 className="slideshow-title">Featured Items</h2>
+      <div className="slideshow">
+        <Slider {...slideshowSettings}>
+          <div className="slideshow-item">
+            <img src="slide1.jpg" alt="Slide 1" />
+            <h3>Slide 1</h3>
+            <p>Content for Slide 1</p>
+          </div>
+          <div className="slideshow-item">
+            <img src="slide2.jpg" alt="Slide 2" />
+            <h3>Slide 2</h3>
+            <p>Content for Slide 2</p>
+          </div>
+          <div className="slideshow-item">
+            <img src="slide3.jpg" alt="Slide 3" />
+            <h3>Slide 3</h3>
+            <p>Content for Slide 3</p>
+          </div>
+        </Slider>
       </div>
     </div>
   );
